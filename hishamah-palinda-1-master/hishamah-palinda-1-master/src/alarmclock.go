98ec482d1bfe473dbacea7e343218b2fa2e2e4a7
Hisham Ahmed <hisham.ahmed@aol.com>
2021-03-09 20:40:59 +0100

    done with alaramclock.go

---
+++ hishamah-palinda-1-master/hishamah-palinda-1-master/src/alarmclock.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func Remind (text string, delay time.Duration) {
+	for {
+		time.Sleep(delay)
+		go fmt.Printf("The time is %v: %v\n", time.Now().Format("15.04.05"), text);
+	}
+}
+
+func main() {
+    go Remind("Time to eat", 10*time.Second)
+	go Remind("Time to work", 30*time.Second)
+	go Remind("Time to sleep", 60*time.Second)
+	select{}
+}
