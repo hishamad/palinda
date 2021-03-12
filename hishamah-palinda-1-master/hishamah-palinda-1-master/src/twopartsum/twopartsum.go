c960b84aeea0336467e294dbcbbec4259c53986b
Hisham Ahmed <hisham.ahmed@aol.com>
2021-03-12 21:50:50 +0100

    Done with the first homework

--- hishamah-palinda-1-master/hishamah-palinda-1-master/src/twopartsum/twopartsum.go
+++ hishamah-palinda-1-master/hishamah-palinda-1-master/src/twopartsum/twopartsum.go
@@ -7,7 +7,12 @@ import (
 // sum the numbers in a and send the result on res.
 func sum(a []int, res chan<- int) {
 	// TODO sum a
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
 	// TODO send result on res
+	res <- sum
 }
 
 // concurrently sum the array a.
@@ -16,9 +21,11 @@ func ConcurrentSum(a []int) int {
 	ch := make(chan int)
 	go sum(a[:n/2], ch)
 	go sum(a[n/2:], ch)
+	sum1 := <- ch
+	sum2 := <- ch
 
 	// TODO Get the subtotals from the channel and return their sum
-	return -1
+	return sum1 + sum2
 }
 
 func main() {
--- hishamah-palinda-1-master/hishamah-palinda-1-master/src/twopartsum/twopartsum_test.go
+++ hishamah-palinda-1-master/hishamah-palinda-1-master/src/twopartsum/twopartsum_test.go
@@ -5,15 +5,24 @@ import (
 )
 
 // test that ConcurrentSum sums an even-length array correctly
-func TestSumConcurrentCorrectlySumsEvenArray(t *testing.T) {
-	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	expected := 55
-
-	actual := ConcurrentSum(arr)
-
-	if actual != expected {
-		t.Errorf("expected %d, was %d", expected, actual)
-	}
+type testStruct struct {
+	elements []int
+	sum int
 }
 
-// TODO add at least two more test cases!
+var tests = []testStruct {
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 55},
+	{[]int{-1, -2, -3, -4, 1, 2, 3, 4}, 0},
+	{[]int{-10, 10, 0, 5, -5, 3, 2}, 5},
+}
+func TestSumConcurrentCorrectlySumsEvenArray(t *testing.T) {
+	for _, test := range tests {
+		expected := test.sum
+		actual := ConcurrentSum(test.elements)
+		if actual != expected {
+			t.Errorf("expected %d, was %d", expected, actual)
+		}
+	}
+
+
+}
