b6b49fbac6f31a5b63d731a5574c41269dea2e4e
Hisham Ahmed <hisham.ahmed@aol.com>
2021-03-14 13:54:14 +0100

    Done with task 1 and 2

--- hishamah-palinda-2-master/hishamah-palinda-2-master/src/bug01.go
+++ hishamah-palinda-2-master/hishamah-palinda-2-master/src/bug01.go
@@ -5,6 +5,10 @@ import "fmt"
 // I want this program to print "Hello world!", but it doesn't work.
 func main() {
 	ch := make(chan string)
-	ch <- "Hello world!"
+	// Error: The program didn't work because it's stuck waiting forever until the data could be recieved. This happens when there is no other goroutine that could recieve the data.
+	// Solution: It's easily fixed by initaiting a new goroutine in order to pass the data through the channel and will be recivied because the program now is running concurrently
+	go func(){
+		ch <- "Hello world!"
+	}()
 	fmt.Println(<-ch)
 }
--- hishamah-palinda-2-master/hishamah-palinda-2-master/src/bug02.go
+++ hishamah-palinda-2-master/hishamah-palinda-2-master/src/bug02.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 // This program should go to 11, but it seemingly only prints 1 to 10.
-func main() {
+
+func main(){ 
+	wg := new(sync.WaitGroup)	
 	ch := make(chan int)
-	go Print(ch)
+	go Print(ch, wg)
+	wg.Add(11)
 	for i := 1; i <= 11; i++ {
 		ch <- i
 	}
 	close(ch)
+	// Error: The program is terminated before it gets to print the last number, 11
+	// Solution: We create a waitGroup that waits for the eleven operations to be done and only after that the program can be terminated
+	wg.Wait()
 }
 
 // Print prints all numbers sent on the channel.
 // The function returns when the channel is closed.
-func Print(ch <-chan int) {
+func Print(ch <-chan int, wg* sync.WaitGroup) {
 	for n := range ch { // reads from channel until it's closed
 		time.Sleep(10 * time.Millisecond) // simulate processing time
 		fmt.Println(n)
+		wg.Done()
 	}
 }
